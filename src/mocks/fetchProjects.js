9bd909db1f1cd180ab362ae8890f21b66c7b6bd9
qinfuji <7748112@qq.com>
2018-07-01 17:35:04 +0800

    dev file tabs

---
+++ src/mocks/fetchProjects.js
@@ -0,0 +1,108 @@
+module.exports.fetchProjects = function(id) {
+  return [
+    {
+      name: 'lggqvq lggqvq',
+      label: 'pariatur nisi labore',
+      id: 'Af99FBeF-D107-9E7C-1fe6-2Ddb6f9e2a6E',
+      logo: 'http://dummyimage.com/250x250',
+      createTime: '1982-08-06 21:15:51',
+      updateTime: '1992-02-14 03:20:20'
+    },
+    {
+      name: 'vffvdhbq vffvdhbq',
+      label: 'qui est ea do culpa',
+      id: 'F3DbdEf1-6d4A-5BfF-Fd85-affaCb1175DF',
+      logo: 'http://dummyimage.com/234x60',
+      createTime: '1974-06-04 08:25:06',
+      updateTime: '2002-05-22 09:46:20'
+    },
+    {
+      name: 'lyunhbnl lyunhbnl',
+      label: 'ea in cillum do',
+      id: 'cdf9BE19-E94D-FC75-fd17-2CE5C6a9Dc62',
+      logo: 'http://dummyimage.com/300x600',
+      createTime: '1993-12-29 03:10:18',
+      updateTime: '1985-11-28 03:40:13'
+    },
+    {
+      name: 'axmlfgq axmlfgq',
+      label: 'irure magna id sed non',
+      id: 'D56B0dfA-59dB-762D-d9d1-92Cfb8BFe94E',
+      logo: 'http://dummyimage.com/125x125',
+      createTime: '1975-07-17 08:27:15',
+      updateTime: '2003-09-29 22:46:02'
+    },
+    {
+      name: 'sfcndn sfcndn',
+      label: 'de',
+      id: '209E6b98-ace7-CDE4-AbA7-F481dcdcdBF1',
+      logo: 'http://dummyimage.com/336x280',
+      createTime: '1974-10-17 07:21:54',
+      updateTime: '2016-11-13 15:54:22'
+    },
+    {
+      name: 'jtof jtof jtof jtof',
+      label: 'commodo Lorem sed',
+      id: 'B77A2EDA-f9C1-f5Ed-b8E2-5EA72Af8bF03',
+      logo: 'http://dummyimage.com/180x150',
+      createTime: '2018-02-13 19:57:05',
+      updateTime: '1999-03-28 06:31:19'
+    },
+    {
+      name: 'fjdxwon fjdxwon',
+      label: 'Ut',
+      id: 'f086DebD-1dB9-f3AF-E854-9b3e7bBeF207',
+      logo: 'http://dummyimage.com/120x240',
+      createTime: '2013-12-08 05:15:56',
+      updateTime: '1985-07-04 15:24:43'
+    },
+    {
+      name: 'vkki vkki vkki vkki',
+      label: 'Excepteur',
+      id: '932b9c1f-3BfB-eE82-672e-c4d3B717eA38',
+      logo: 'http://dummyimage.com/160x600',
+      createTime: '1976-06-06 00:21:11',
+      updateTime: '1990-04-25 22:37:45'
+    },
+    {
+      name: 'hvic hvic hvic hvic',
+      label: 'veniam dolor',
+      id: '5Ff6b4dA-02cc-DFEd-7eDc-BA7ce94D2EE6',
+      logo: 'http://dummyimage.com/120x240',
+      createTime: '1975-11-11 03:34:18',
+      updateTime: '2007-03-23 06:28:59'
+    },
+    {
+      name: 'dec dec dec dec',
+      label: 'voluptate',
+      id: 'ee5bAf1F-8942-b3B4-360f-378971195Ad8',
+      logo: 'http://dummyimage.com/336x280',
+      createTime: '2013-12-14 08:45:28',
+      updateTime: '1988-08-16 12:53:41'
+    },
+    {
+      name: 'jakgmdmd jakgmdmd',
+      label: 'culpa cupidatat officia anim aliquip',
+      id: '9efedd0A-b7AB-82Dd-bbbA-fBB0A0ae9328',
+      logo: 'http://dummyimage.com/120x600',
+      createTime: '2006-10-02 17:38:25',
+      updateTime: '1975-10-05 19:59:59'
+    },
+    {
+      name: 'sgfefkyqf sgfefkyqf',
+      label: 'nisi',
+      id: 'd158be34-6D2e-1Bb5-242F-71392eC7b583',
+      logo: 'http://dummyimage.com/240x400',
+      createTime: '1973-01-21 22:42:18',
+      updateTime: '2007-11-18 05:01:51'
+    },
+    {
+      name: 'hude hude hude hude',
+      label: 'aliquip pariatur ullamco',
+      id: 'de93841A-FB6A-4461-9DAB-593A15dea259',
+      logo: 'http://dummyimage.com/720x300',
+      createTime: '1975-07-21 12:24:48',
+      updateTime: '1983-06-15 02:01:45'
+    }
+  ];
+};
